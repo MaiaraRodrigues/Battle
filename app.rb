ac1b0c92adea8458f6dc72325b58e89d812b33da
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-10-13 15:55:24 +0100

    Params added

--- app.rb
+++ app.rb
@@ -10,7 +10,16 @@ get '/' do
     "I like rainbows"
   end
 
-  get '/cat' do
+  get '/random-cat' do
     @name = ["Amigo", "Oscar", "Viking"].sample
+    erb(:index)
+  end
+
+
+  get '/named-cat' do
+    p params
+    @name = params[:name]
+    @last_name = params[:last_name]
+    @colour = params[:colour]
     erb(:index)
   end
