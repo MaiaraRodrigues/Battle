598d70dab9150b227e4e5175da43ea3ed7ad32f9
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-10-13 14:27:38 +0100

    Adding HTML and CSS

--- app.rb
+++ app.rb
@@ -1,10 +1,19 @@
 require 'sinatra'
 require 'shotgun'
+set :session_secret, 'super secret'
 
 get '/' do
-    "Hello!"
+    "Whatever"
   end
 
   get '/secret' do
     "I like rainbows"
   end
+
+  get '/cat' do
+   "<div style='border: 2px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
+  end
+
+ 
