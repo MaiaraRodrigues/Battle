7e375799da912509ddf4cf97153841c790b6705d
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-10-13 17:07:31 +0100

    Sinatra post ed params

--- app.rb
+++ app.rb
@@ -15,11 +15,15 @@ get '/' do
     erb(:index)
   end
 
+  get '/cat_form' do
+    erb :cat_form
+  end
 
-  get '/named-cat' do
+  post '/named-cat' do
     p params
     @name = params[:name]
     @last_name = params[:last_name]
     @colour = params[:colour]
     erb(:index)
   end
+
