e78775f5420d041bdc0ee4aba0cf4863d7e614a1
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-10-14 16:50:38 +0100

    Refactored POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -1,17 +1,24 @@
 require 'sinatra'
 require 'shotgun'
-set :session_secret, 'super secret'
 
 class Battle < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @Player1_Name = params[:Player1_Name]
-    @Player2_Name = params[:Player2_Name]
-    erb(:play)
+    session[:Player1_Name] = params[:Player1_Name]
+    session[:Player2_Name] = params[:Player2_Name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @Player1_Name = session[:Player1_Name]
+    @Player2_Name = session[:Player2_Name]
+    erb :play
   end
 
   # get '/names' do
