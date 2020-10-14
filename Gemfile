f60287ae31feacd8f630a57eb7c772f416cdc9ab
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-14 15:30:15 +0100

    Create Battle app and initialize rspec

--- Gemfile
+++ Gemfile
@@ -4,9 +4,9 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+gem 'rspec'
 gem "sinatra"
 gem "shotgun"
 gem 'capybara'
 gem "selenium-webdriver"
 gem 'chromedriver-helper'
-
--- app.rb
+++ app.rb
@@ -2,28 +2,31 @@ require 'sinatra'
 require 'shotgun'
 set :session_secret, 'super secret'
 
-get '/' do
-    "Whatever"
+class Battle < Sinatra::Base
+
+  get '/' do
+    "Hello battle!"
   end
 
-  get '/secret' do
-    "I like rainbows"
-  end
+end
 
-  get '/random-cat' do
-    @name = ["Amigo", "Oscar", "Viking"].sample
-    erb(:index)
-  end
-
-  get '/cat_form' do
-    erb :cat_form
-  end
-
-  post '/named-cat' do
-    p params
-    @name = params[:name]
-    @last_name = params[:last_name]
-    @colour = params[:colour]
-    erb(:index)
-  end
-
+  # get '/secret' do
+  #   "I like rainbows"
+  # end
+  #
+  # get '/random-cat' do
+  #   @name = ["Amigo", "Oscar", "Viking"].sample
+  #   erb(:index)
+  # end
+  #
+  # get '/cat_form' do
+  #   erb :cat_form
+  # end
+  #
+  # post '/named-cat' do
+  #   p params
+  #   @name = params[:name]
+  #   @last_name = params[:last_name]
+  #   @colour = params[:colour]
+  #   erb(:index)
+  # end
